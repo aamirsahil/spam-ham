c6980283eea7a8f69e4ef80b3848ad0359b1ea81
Aamir Sahil <notifysahil@gmail.com>
2024-08-17 00:01:52 +0530

    Text cleaner

---
+++ text_cleaner/text_cleaner.py
@@ -0,0 +1,9 @@
+class TextCleaner:
+    def __init__(self) -> None:
+        pass
+    def clean_text(self):
+        pass
+    def text_stemmer(self):
+        pass
+    def text_lametizer(self):
+        pass
